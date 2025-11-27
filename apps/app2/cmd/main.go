bbe74bac7749ffe07234c47ffc3c4a3be2185ed5
DarylvdBerg <darylvdberg@gmail.com>
2025-11-27 14:24:36 +0100

    feat: update to app2

--- apps/app2/cmd/main.go
+++ apps/app2/cmd/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
+
 	someshareddep "github.com/DarylvdBerg/mono-release-me-poc/shared/some-shared-dep"
 )
 
 func main() {
 	someshareddep.Hello()
+	fmt.Print("HELLo")
 }
