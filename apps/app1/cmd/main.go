7a70e03d72275e08c0f74ebf81e955490cb007d0
DarylvdBerg <darylvdberg@gmail.com>
2025-11-28 15:07:51 +0100

    feat update update update

--- apps/app1/cmd/main.go
+++ apps/app1/cmd/main.go
@@ -14,4 +14,6 @@ func main() {
 	fmt.Print("Look another change!")
 	fmt.Print("Look another change!")
 	fmt.Print("Look another change!")
+	fmt.Print("Look another change!")
+	fmt.Print("Look another change!")
 }
